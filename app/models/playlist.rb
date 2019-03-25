f9735c2540ef78df022c68bd55579a4dcbd6eb12
NickyEXE <dovern42@gmail.com>
2019-03-25 15:03:23 -0400

    removed necessity for author_id and cleaned up some playlist features

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -4,4 +4,12 @@ class Playlist < ApplicationRecord
   has_many :playlist_songs
   has_many :songs, through: :playlist_songs
 
+  def original_author
+    User.find(Follow.all.where(playlist_id: self.id).order(:created_at).first.user_id)
+  end
+
+  def original_author_dj_name
+    self.original_author.dj_name
+  end
+
 end
