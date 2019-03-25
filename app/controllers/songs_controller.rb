42b98aa78640502325fefd3335211f15e5d7a39f
NickyEXE <dovern42@gmail.com>
2019-03-25 17:37:16 -0400

    fixed all the routes and made it restful

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -7,9 +7,9 @@ class SongsController < ApplicationController
 
   # GET /songs
   # GET /songs.json
-  def index
-    @songs = Song.all
-  end
+  # def index
+  #   @songs = Song.all
+  # end
 
   # GET /songs/1
   # GET /songs/1.json
@@ -64,7 +64,7 @@ class SongsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_song
-      @song = Song.find(params[:id])
+      @song = Song.find(params[:sid])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :songs
+  get '/playlists/:pid/songs/:sid/edit', to: 'songs#edit', as: 'edit_song'
+  patch '/playlists/:pid/songs/:sid', to: 'songs#update'
+  delete '/playlists/:pid/songs/:sid/destroy', to: 'songs#destroy', as: 'destroy_song'
+  get '/playlists/:pid/songs/:sid', to: 'songs#show', as: 'song'
+  get '/playlists/:pid/songs/new', to: 'songs#new', as: 'new_song'
   resources :playlists
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
