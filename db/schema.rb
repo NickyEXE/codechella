a414636b8ec8ed944d580ef652f4083ccaa30f3d
NickyEXE <dovern42@gmail.com>
2019-03-25 15:23:56 -0400

    done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_25_150458) do
+ActiveRecord::Schema.define(version: 2019_03_25_192245) do
 
   create_table "follows", force: :cascade do |t|
     t.integer "user_id"
@@ -29,7 +29,6 @@ ActiveRecord::Schema.define(version: 2019_03_25_150458) do
   create_table "playlists", force: :cascade do |t|
     t.string "name"
     t.string "description"
-    t.integer "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
