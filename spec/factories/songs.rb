8546b79ba1b91eabc29df19e26c74b0bfad0c808
rrosztoczy <rrosztoczy@gmail.com>
2019-03-27 12:06:12 -0400

    Added Create Playlist Test

---
+++ spec/factories/songs.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+    factory :song do
+        name {"MySong"}
+        artist {"MyArtist"}
+        lyrics {"These are my lyrics"}
+        genre {"MyGenre"}
+        year {2000}
+    end
+  end
