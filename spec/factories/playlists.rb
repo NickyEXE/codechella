302a792c43144ce660fb22875b9b60ca376ea04b
rrosztoczy <rrosztoczy@gmail.com>
2019-03-27 14:17:35 -0400

    added follow/unfollow tests

--- spec/factories/playlists.rb
+++ spec/factories/playlists.rb
@@ -1,6 +1,21 @@
 FactoryBot.define do
+    factory :follow do
+        user
+        playlist
+      end
+
     factory :playlist do
         name { "MyPlaylist" }
         description { "This is a test playlist" }
+
+        factory :playlist_with_follows do
+            transient do
+                follows_count { 3 }
+            end
+
+            after :create do |playlist, evaluator|
+                create_list(:follow, evaluator.follows_count, playlist: playlist)
+            end
+        end
     end
   end
