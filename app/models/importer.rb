9cf66083553677d396682a8e1e1dc9bfa94b70ba
NickyEXE <dovern42@gmail.com>
2019-03-28 16:31:35 -0400

    fixed playlists and added spotify playlist button

--- app/models/importer.rb
+++ app/models/importer.rb
@@ -6,9 +6,28 @@ class Importer
     album = track.album.name
     url = track.external_urls.values.first
     popularity = track.popularity
-    year = track.album.release_date[0..3].to_i
-    image_url = track.album.images.first["url"]
+    year = year_getter(track)
+    image_url = image_getter(track)
     {name: name, artist: artist, album: album, url: url, popularity: popularity, year: year, image_url: image_url}
+  end
+
+
+  def year_getter(track)
+    if track.respond_to?(:album)
+      if track.album.release_date
+        track.album.release_date[0..3].to_i
+      end
+    end
+  end
+
+  def image_getter(track)
+    if track.respond_to?(:album)
+      if track.album.images
+        if track.album.images.first
+          track.album.images.first["url"]
+        end
+      end
+    end
   end
 
   def tracks_to_hash_array
